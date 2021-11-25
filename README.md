f0ee83b01162838ced9f6d73f59b66405b5dd52f
commonPlaceC <aalex.03@mail.ru>
2021-11-25 14:03:19 +0300

    upadate

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-###Genetic algorithm of finding the maximum value of a function
+Genetic algorithm of finding the maximum value of a function
 ---
 <img src="https://img.shields.io/badge/-C%2B%2B-blue?&logo=c%2B%2B">
 
-University project by commonPlaceC & CpedniyNikon
+University project by commonPlaceC
 
 Finding the local maximum of a functions:
 
-- <img src="src/1.png" width=50> with maximum 250 for x ∈ [0; 500)
-- <img src="src/2.png" width=60> with maximum 9900 in x ∈ [0; 100)
+- <img src="src/1.png" width=30> with maximum 250 for x ∈ [-250; 250]
+- <img src="src/2.png" width=200> with maximum 63000 in x ∈ [-250; 250]
--- commonPlaceC.cpp
+++ commonPlaceC.cpp
@@ -3,57 +3,89 @@
 #include <algorithm>
 #include <tuple>
 #include <cmath>
+#include <conio.h>
+#include <windows.h>
 #include <chrono>
 
+//Определение переменых
+#define x get<0>(parent)
+#define y get<1>(parent)
+#define z get<2>(parent)
+
 using namespace std;
-using namespace std::chrono;
+using namespace std::chrono; //Подсчет времени
 
 using person = tuple<int, int, int>;
 using populations = vector<person>;
 
 
+//Создание структуры книг
+int userInput() {
+    int num;
+    while(!(cin >> num)) {
+        cin.clear();
+        cin.ignore(32276,'\n');
+        cout << "Wrong value! Please enter again:  ";
+    };
+    return num;
+}
+
 //Функция
-int fitness(person &parent) {
-	if (get<1>(parent) != 0) 
-		// return ((get<0>(parent) - get<2>(parent)) / (2 * get<1>(parent))); // (x - z) / 2 * y
-		return (get<2>(parent) - get<0>(parent) + get<0>(parent) * get<1>(parent)); // z-x + xy | z - x*(1 - y)
+int fitness(const person &parent, const __int16 &funcNum) {
+	if (y != 0 && funcNum == 1) 
+		return (x - z) / (2 * y); // (x - z) / 2 * y  250
+	else if (funcNum == 2)
+		return (z - x + x * y); // z-x + xy | z - x*(1 - y)  63000
+	else
+		return INT_MIN;
 }
 
 
 //Создание первой популяции
-void createNewPopulation(populations &population_)
-{
+void createNewPopulation(populations &population_) {
 	srand((int)time(NULL));
 	for (int i = 0; i < 1500; i++) {
-		population_.push_back(make_tuple(rand() % 500 - 250, rand() % 500 - 250, rand() % 500 - 250));
+		population_.push_back(make_tuple(rand() % 501 - 250, rand() % 501 - 250, rand() % 501 - 250));
 	}
 }
 
-//Скрещивание генов
+
+//Кроссинговер
 void geneCrossing(populations &population_, person parent1, person parent2) {
 	srand((int)time(NULL)); 
 	population_.push_back(make_tuple(get<0>(parent1), get<1>(parent2), get<2>(parent2)));
 	population_.push_back(make_tuple(get<0>(parent1), get<1>(parent1), get<2>(parent2)));
 	population_.push_back(make_tuple(get<0>(parent2), get<1>(parent1), get<2>(parent1)));
 	population_.push_back(make_tuple(get<0>(parent2), get<1>(parent2), get<2>(parent1)));
-	//Мутации
-	if (rand() % 5 == 0)
-		population_.push_back(make_tuple(rand() % 500 - 250, get<1>(parent1), get<2>(parent2)));
-	if (rand() % 5 == 0)
-		population_.push_back(make_tuple(rand() % 500 - 250, get<1>(parent2), get<2>(parent1)));
-	if (rand() % 5 == 0)
-		population_.push_back(make_tuple(get<0>(parent1), rand() % 500 - 250, get<2>(parent2)));
-	if (rand() % 5 == 0)
-		population_.push_back(make_tuple(get<0>(parent2), rand() % 500 - 250, get<2>(parent1)));
-	if (rand() % 5 == 0)
-		population_.push_back(make_tuple(get<0>(parent1), get<1>(parent2), rand() % 500 - 250));
-	if (rand() % 5 == 0)
-		population_.push_back(make_tuple(get<0>(parent2), get<1>(parent1), rand() % 500 - 250));
+	//Мутации c вероятностью 25%)
+	{
+	if (rand() % 4 == 0) //Мутации c вероятностью 25%)
+		population_.push_back(make_tuple(rand() % 501 - 250, get<1>(parent1), get<2>(parent2)));
+	if (rand() % 4 == 0)
+		population_.push_back(make_tuple(rand() % 501 - 250, get<1>(parent2), get<2>(parent1)));
+	if (rand() % 4 == 0)
+		population_.push_back(make_tuple(get<0>(parent1), rand() % 501 - 250, get<2>(parent2)));
+	if (rand() % 4 == 0)
+		population_.push_back(make_tuple(get<0>(parent2), rand() % 501 - 250, get<2>(parent1)));
+	if (rand() % 4 == 0)
+		population_.push_back(make_tuple(get<0>(parent1), get<1>(parent2), rand() % 501 - 250));
+	if (rand() % 4 == 0)
+		population_.push_back(make_tuple(get<0>(parent2), get<1>(parent1), rand() % 501 - 250));
+	}
 
 }
 
+//Селекция
+void selection(populations &population_, const __int16 &funcNum) {
+	sort(population_.begin(), population_.end(), [&](person parent1, person parent2) { return fitness(parent1, funcNum) > fitness(parent2, funcNum); });
+
+	while (population_.size() > 1500) {
+		population_.pop_back();
+	}
+}
+
 //Размножение
-void reproduction(populations &population_) {
+void reproduction(populations &population_, const __int16 &funcNum) {
 
 	srand((int)time(NULL));
 	size_t size_ = population_.size();
@@ -62,33 +94,34 @@ void reproduction(populations &population_) {
 		geneCrossing(population_, population_[rand() % population_.size()], population_[rand() % population_.size()]);
 	}
 
-	sort(population_.begin(), population_.end(), [](person parent1, person parent2) { return fitness(parent1) > fitness(parent2); });
-
-	while (population_.size() > 1500) {
-		population_.pop_back();
-	}
+	selection(population_, funcNum);
 }
 
 
 //Главная функция
 int main() {
 
+	cout << "Please enter the number of function: (1/2): ";
+	__int16 funcNum = userInput();
+
 	auto start = high_resolution_clock::now();
 
-	populations population_;
+	populations population_; 
 	createNewPopulation(population_);
 
-	int try_ = 100;
+	__int16 try_ = 120; //Количесвто итераций
 	while (try_) {
-		reproduction(population_);
+		reproduction(population_, funcNum);
 		try_--;
 	}
-	cout << "Final result of algorithm: " << fitness(population_[population_.size() - 1]) << endl;
-	cout << "With values: " << get<0>(population_[population_.size() - 1]) << " " << get<1>(population_[population_.size() - 1]) << " " << get<2>(population_[population_.size() - 1]) << endl;
 
+	cout << "\tFinal result of algorithm: " << fitness(population_[0], funcNum) << endl;
 	auto stop = high_resolution_clock::now();
-	auto time = duration_cast<microseconds>(stop - start);
-	cout << time.count() / 1000000.0 << " - seconds taken";
+	auto time = duration_cast<milliseconds>(stop - start);
+	cout << "\tWith values: x = " << get<0>(population_[0]);
+	cout << " y = " << get<1>(population_[0]);
+	cout << " z = " << get<2>(population_[0]) << endl;
+	cout << time.count() / 1000.0 << " - seconds taken";
 
 
 	return 0;
